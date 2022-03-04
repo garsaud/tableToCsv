2f29773a6ba2f598cf7e5fc4af725228c2467716
Cyril Garsaud <cyril.garsaud@gmail.com>
2022-03-05 00:13:41 +0100

    Version 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # tableToCsv
 Simply convert a table into a CSV representation
+
+## Example
+
+```html
+<table>
+    <thead>
+        <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Age</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Eustache</td>
+            <td>Beaugenoux</td>
+            <td>65</td>
+        </tr>
+        <tr>
+            <td>Aristide</td>
+            <td>Rouquitourne</td>
+            <td>46</td>
+        </tr>
+        <tr>
+            <td>Gersande</td>
+            <td>De Ronflefort</td>
+            <td>78</td>
+        </tr>
+    </tbody>
+</table>
+```
+
+```js
+// ES module
+import TableToCsv from 'tableToCsv.js';
+
+// Common JS
+const TableToCsv = require('tableToCsv.js');
+
+const csv = new TableToCsv(document.querySelector('table'));
+csv.download();
+```
+
+A few options are available:
+
+```js
+const csv = new TableToCsv(node, {
+    filename: 'table.csv',
+    cellSeparator: ',',
+    rowSeparator: '\n',
+});
+```
+
+If the table changes after initialization, you can refresh programmatically:
+
+```js
+const csv = new TableToCsv(…);
+
+// table changes...
+
+csv.refresh();
+csv.download();
+```
---
+++ tableToCsv.js
@@ -0,0 +1,74 @@
+;(function (that) {
+
+    function TableToCsv(table, options) {
+        var _options = Object.assign({
+            'filename': 'table.csv',
+            'cellSeparator': ',',
+            'rowSeparator': '\n',
+        }, options);
+
+        var _textContent = '';
+        var _table = table;
+
+        function escape(text) {
+            return text
+                .replace('"','""')
+                .replace(/[\r\n]+/g,' ');
+        }
+
+        function downloadCSVFile(csv, filename) {
+            var csv_file = new Blob([csv], {type: 'text/csv'});
+            var anchor = document.createElement('a');
+            anchor.download = filename;
+            anchor.href = window.URL.createObjectURL(csv_file);
+            document.body.appendChild(anchor);
+
+            anchor.click();
+        }
+
+        var interface = {
+            refresh() {
+                var tableRows = _table.querySelectorAll('tr');
+
+                var textRows = [].slice.call(tableRows).map(function (tableRow) {
+                    var row = [];
+                    var tableCells = tableRow.querySelectorAll('td, th');
+
+                    var textCells = [].slice.call(tableCells).map(function (tableCell) {
+                        return '"'+escape(tableCell.innerText)+'"';
+                    });
+
+                    return textCells.join(_options.cellSeparator);
+                });
+
+                _textContent = textRows.join(_options.rowSeparator);
+            },
+            download(filename) {
+                return downloadCSVFile(_textContent, filename || _options.filename);
+            },
+        };
+
+        interface.refresh();
+
+        return interface;
+    }
+
+    var loadedModule = TableToCsv;
+
+    loadedModule['default'] = loadedModule.TableToCsv = loadedModule;
+
+    // AMD
+    if (typeof define === 'function' && define.amd) {
+        define(function() {
+            return loadedModule;
+        });
+    }
+    // Node and other CommonJS-like environments that support module.exports
+    else if (typeof module !== 'undefined' && module.exports) {
+        module.exports = loadedModule;
+    }
+    // Browser
+    else {
+        that.TableToCsv = loadedModule;
+    }
+})(this);
